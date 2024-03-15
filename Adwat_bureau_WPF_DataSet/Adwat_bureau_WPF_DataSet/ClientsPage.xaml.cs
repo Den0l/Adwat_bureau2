d20d0c6a67574e819aacf331bf954ffe598068c2
Den0l <denol02.02.06@gmail.com>
2024-03-15 21:09:27 +0300

    com

---
+++ Adwat_bureau_WPF_DataSet/Adwat_bureau_WPF_DataSet/ClientsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Adwat_bureau_WPF_DataSet.Adwat_bureau_DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Adwat_bureau_WPF_DataSet
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientsPage.xaml
+    /// </summary>
+    public partial class ClientsPage : Page
+    {
+        ClientsTableAdapter clients = new ClientsTableAdapter();
+        public ClientsPage()
+        {
+            InitializeComponent();
+            grid_Clients.ItemsSource = clients.GetData();
+        }
+    }
+}
---
+++ Adwat_bureau_WPF_DataSet/Adwat_bureau_WPF_DataSet/LawyerPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Adwat_bureau_WPF_DataSet.Adwat_bureau_DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Adwat_bureau_WPF_DataSet
+{
+    /// <summary>
+    /// Логика взаимодействия для LawyerPage.xaml
+    /// </summary>
+    public partial class LawyerPage : Page
+    {
+        LawyerTableAdapter lawyers = new LawyerTableAdapter();
+
+        public LawyerPage()
+        {
+            InitializeComponent();
+            grid_Lawyer.ItemsSource = lawyers.GetData();
+        }
+    }
+}
